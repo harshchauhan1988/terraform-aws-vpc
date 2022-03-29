c881138c4dcdfe48095f6d0780bbe9ccb3fb59a0
Harshvardhan Chauhan <harshvardhac@harshvardha-a01.vmware.com>
2022-03-29 08:31:52 +0530

    VPC Code

--- examples/simple-vpc/main.tf
+++ examples/simple-vpc/main.tf
@@ -1,40 +1,27 @@
-provider "aws" {
-  region = local.region
-}
-
 locals {
-  region = "eu-west-1"
+  cluster_name = "my-cluster"
 }
-
-################################################################################
-# VPC Module
-################################################################################
-
+data "aws_availability_zones" "available" {
+}
 module "vpc" {
-  source = "../../"
+  source  = "terraform-aws-modules/vpc/aws"
 
-  name = "simple-example"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_ipv6 = true
-
-  enable_nat_gateway = false
-  single_nat_gateway = true
+  name                 = "k8s-vpc"
+  cidr                 = "172.16.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
 
   public_subnet_tags = {
-    Name = "overridden-name-public"
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
   }
 
-  tags = {
-    Owner       = "user"
-    Environment = "dev"
-  }
-
-  vpc_tags = {
-    Name = "vpc-name"
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
   }
 }
